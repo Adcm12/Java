a8df708481bda04d75798cd83075a3f1c81039a0
Adrian Castillo <david.marcanao07@gmail.com>
2026-04-07 16:00:53 -0300

    commit

---
+++ OEE-Calculator/src/com/portfolio/oee/OEE_Calculator.java
@@ -0,0 +1,12 @@
+package com.portfolio.oee;
+
+public class OEE_Calculator {
+
+	public static void main(String[] args) {
+		
+		System.out.println("HOla");
+		// TODO Auto-generated method stub
+
+	}
+
+}
